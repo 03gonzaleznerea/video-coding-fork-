7bd3112a3f53b1f3f0cfaf9750a6da68c121c90f
03gonzaleznerea <115104000+03gonzaleznerea@users.noreply.github.com>
2024-12-13 16:36:37 +0100

    Delete practice1 directory

--- practice1/app/services/resize_image.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-def resize_image(input_path, output_path, width, height):
-    command = ['ffmpeg', '-i', input_path, '-vf', f'scale={width}:{height}', output_path]
-    subprocess.run(command, check=True)
